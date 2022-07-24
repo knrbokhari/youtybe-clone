ed24a3c3f18ebe6174d4a4e979686633b6da1b12
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-24 17:15:03 +0600

    creating menu for the home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>video</Wrapper>
+      </Main>
+    </Container>
   );
 }
 
