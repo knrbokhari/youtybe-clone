14522679e9fee7dd8d64bf74c301d4b264309b61
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-24 07:55:29 +0600

    updating updateVideo route

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -8,6 +8,7 @@ import {
   search,
   sub,
   trend,
+  updateVideo,
 } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
@@ -15,14 +16,32 @@ const router = express.Router();
 
 //create a video
 router.post("/", verifyToken, addVideo);
-router.put("/:id", verifyToken, addVideo);
+
+// update a video
+router.put("/:id", verifyToken, updateVideo);
+
+// delete a video
 router.delete("/:id", verifyToken, addVideo);
+
+// get a video
 router.get("/find/:id", getVideo);
+
+// adding View to a video
 router.put("/view/:id", addView);
+
+// get trend video
 router.get("/trend", trend);
+
+// get rendom video
 router.get("/random", random);
+
+// get sub video
 router.get("/sub", verifyToken, sub);
+
+// get video tags
 router.get("/tags", getByTag);
+
+// search video by name
 router.get("/search", search);
 
 export default router;
