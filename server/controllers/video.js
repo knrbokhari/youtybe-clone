e3218e1f821d8bfe757da0417cc9dda8d11395e9
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-24 08:46:44 +0600

    updating getByTag route

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -103,6 +103,14 @@ export const sub = async (req, res, next) => {
   }
 };
 
-export const getByTag = async (req, res, next) => {};
+export const getByTag = async (req, res, next) => {
+  const tags = req.query.tags.split(",");
+  try {
+    const videos = await Video.find({ tags: { $in: tags } }).limit(20);
+    res.status(200).json(videos);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const search = async (req, res, next) => {};
