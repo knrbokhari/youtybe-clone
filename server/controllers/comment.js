075578b5807569397a3307e633296157747ac1b1
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-24 09:04:26 +0600

    updating deleteComment route

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,3 +1,7 @@
+import Video from "../models/Video";
+import Comment from "../models/Comment.js";
+import { createError } from "../error.js";
+
 export const addComment = async (req, res, next) => {
   const newComment = new Comment({ ...req.body, userId: req.user.id });
   try {
@@ -7,5 +11,18 @@ export const addComment = async (req, res, next) => {
     next(err);
   }
 };
-export const deleteComment = async (req, res, next) => {};
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(res.params.id);
+    const video = await Video.findById(res.params.id);
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("The comment has been deleted.");
+    } else {
+      return next(createError(403, "You can delete ony your comment!"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 export const getComments = async (req, res, next) => {};
