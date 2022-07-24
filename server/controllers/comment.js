f766620f5a5d31d00064a22487ede0bbac0c08b7
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-24 08:59:39 +0600

    updating addComment route

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,3 +1,11 @@
-export const addComment = async (req, res, next) => {};
+export const addComment = async (req, res, next) => {
+  const newComment = new Comment({ ...req.body, userId: req.user.id });
+  try {
+    const savedComment = await newComment.save();
+    res.status(200).send(savedComment);
+  } catch (err) {
+    next(err);
+  }
+};
 export const deleteComment = async (req, res, next) => {};
 export const getComments = async (req, res, next) => {};
