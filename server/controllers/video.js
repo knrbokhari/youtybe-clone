11572b81f6a657f97a0c1a2814065af59e4bed6d
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-24 08:02:07 +0600

    updating random route

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -77,7 +77,14 @@ export const trend = async (req, res, next) => {
   }
 };
 
-export const random = async (req, res, next) => {};
+export const random = async (req, res, next) => {
+  try {
+    const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+    res.status(200).json(videos);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const sub = async (req, res, next) => {};
 
