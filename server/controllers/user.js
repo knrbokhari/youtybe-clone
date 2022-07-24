c61ebf6e3f4893ccdba6dac2ecb02c4430fdf4fe
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-24 06:57:19 +0600

    creating user Update route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,24 @@
-export const updateUser = (req, res) => {
-  res.json("");
+import { createError } from "../error.js";
+import User from "../models/User.js";
+import Video from "../models/Video.js";
+
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You can update only your account!"));
+  }
 };
 
 export const deleteUser = (req, res) => {
