794ed7001f5c3faf65fc3ac4f579d79e9110ca6c
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-26 15:47:50 +0600

    creating videoSlice

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
 import MyTube from "../img/logo.png";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   position: sticky;
@@ -96,6 +97,10 @@ const Avatar = styled.img`
 `;
 
 const Navbar = () => {
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const [open, setOpen] = useState(false);
+  // console.log(currentUser);
+
   return (
     <Container>
       <Wrapper>
@@ -109,12 +114,20 @@ const Navbar = () => {
           <Input placeholder="Search" />
           <SearchOutlinedIcon />
         </Search>
-        <Link to="/signin" style={{ textDecoration: "none", color: "inherit" }}>
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
-        </Link>
+        {currentUser ? (
+          <User>
+            <VideoCallOutlinedIcon onClick={() => setOpen(true)} />
+            <Avatar src={currentUser?.img} />
+            {currentUser?.name}
+          </User>
+        ) : (
+          <Link to="signin" style={{ textDecoration: "none" }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>
+        )}
       </Wrapper>
     </Container>
   );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import videoReducer from "./videoSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: userReducer,
+    user: userReducer,
+    video: videoReducer,
   },
 });
